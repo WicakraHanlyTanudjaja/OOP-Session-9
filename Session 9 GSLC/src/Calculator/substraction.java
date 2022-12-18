4c27331b9ee165f8c483ee69b11a9858b70fdbcc
WicakraHanlyTanudjaja <114389774+WicakraHanlyTanudjaja@users.noreply.github.com>
2022-12-18 20:13:37 +0700

    Update substraction.java

--- Session 9 GSLC/src/Calculator/substraction.java
+++ Session 9 GSLC/src/Calculator/substraction.java
@@ -1,5 +1,6 @@
 package Calculator;
 
+// Untuk menampilkan hasil pengurangan dari 2 bilangan
 public class substraction extends Calculate implements CalculateInterface {
 
 	public substraction(double a, double b) {
@@ -7,6 +8,7 @@ public class substraction extends Calculate implements CalculateInterface {
 		// TODO Auto-generated constructor stub
 	}
 
+	// Untuk menampilkan hasil dari a - b
 	@Override
 	public double calculate() {
 		// TODO Auto-generated method stub
