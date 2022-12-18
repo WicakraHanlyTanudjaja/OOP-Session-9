146d7c65cc10f92dfca0fe0fb832f778c3f50537
WicakraHanlyTanudjaja <114389774+WicakraHanlyTanudjaja@users.noreply.github.com>
2022-12-18 20:12:56 +0700

    Update division.java

--- Session 9 GSLC/src/Calculator/division.java
+++ Session 9 GSLC/src/Calculator/division.java
@@ -1,5 +1,6 @@
 package Calculator;
 
+// Untuk menampilkan hasil pembagian antar 2 bilangan
 public class division extends Calculate implements CalculateInterface {
 
 	public division(double a, double b) {
@@ -7,6 +8,7 @@ public class division extends Calculate implements CalculateInterface {
 		// TODO Auto-generated constructor stub
 	}
 
+	// Untuk menampilkan hasil a bagi b
 	@Override
 	public double calculate() {
 		// TODO Auto-generated method stub
