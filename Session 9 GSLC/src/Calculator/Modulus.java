cf6c8067724967ab23f7a7a0caa853de1b3238f0
WicakraHanlyTanudjaja <114389774+WicakraHanlyTanudjaja@users.noreply.github.com>
2022-12-18 20:09:22 +0700

    Update Modulus.java

--- Session 9 GSLC/src/Calculator/Modulus.java
+++ Session 9 GSLC/src/Calculator/Modulus.java
@@ -1,5 +1,6 @@
 package Calculator;
 
+// Untuk menampilkan sisa bagi dari 2 bilangan
 public class Modulus extends Calculate implements CalculateInterface {
 
 	public Modulus(double a, double b) {
@@ -7,6 +8,7 @@ public class Modulus extends Calculate implements CalculateInterface {
 		// TODO Auto-generated constructor stub
 	}
 
+	// Untuk menjalankan metode dari interface sebelumnya
 	@Override
 	public double calculate() {
 		// TODO Auto-generated method stub
